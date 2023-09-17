da917331c841e3fb232721047134458fa40ac2fe
sashkomatviychuk <sashko1921@gmail.com>
2023-09-17 14:12:40 +0300

    Changes

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 
 const express = require('express');
+const os = require('os');
+
 const app = express();
 const port = process.env.PORT;
+
+const getIp = () => {
+  const networkInterfaces = os.networkInterfaces();
+
+  const ipv4Address = Object.values(networkInterfaces)
+    .flat()
+    .find((iface) => !iface.internal && iface.family === 'IPv4');
+
+  return ipv4Address;
+};
 
 app.get('/', (req, res) => {
   res.json({
     status: 'OK',
+    ip: getIp().address,
   });
 });
 
